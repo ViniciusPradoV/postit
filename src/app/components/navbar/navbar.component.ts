4e35a3332ecd7c47a52a0e358c9f526d15cf488a
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2022-07-17 14:59:46 -0300

    refactored navbar.component.html to work with a for loop

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/internal/operators/filter';
 import { NavbarEnum } from 'src/app/models/enums/navbar.enum';
+import { NavbarItemInterface } from 'src/app/models/interfaces/navbar-item.interface';
 
 @Component({
   selector: 'app-navbar',
@@ -25,14 +26,30 @@ export class NavbarComponent {
           this.currentNavbar = NavbarEnum.HOME;
           if(this.router.url.includes('/profile'))
           this.currentNavbar = NavbarEnum.PROFILE;
-
-    })
-
-    
+    });
   }
 
   public navbarEnum: typeof NavbarEnum = NavbarEnum;
   public currentNavbar: NavbarEnum = NavbarEnum.HOME;
 
-
+  public navbarList: NavbarItemInterface[] = [
+    {
+      type: NavbarEnum.FEED,
+      link: '/feed',
+      icon: 'assets/imgs/feed_disabled.svg',
+      iconActivated: 'assets/imgs/feed_enabled.svg',
+    },
+    {
+      type: NavbarEnum.HOME,
+      link: '/home',
+      icon: 'assets/imgs/home_disabled.svg',
+      iconActivated: 'assets/imgs/home_enabled.svg',
+    },
+    {
+      type: NavbarEnum.PROFILE,
+      link: '/profile',
+      icon: 'assets/imgs/user_disabled.svg',
+      iconActivated: 'assets/imgs/user_enabled.svg',
+    },
+  ]
 }
