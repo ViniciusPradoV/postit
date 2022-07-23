957c454eaca7f8b87dbe6c2f091f6c261a19e455
Vinicius do Prado Vieira <31973401+ViniciusPradoV@users.noreply.github.com>
2022-07-23 08:46:53 -0300

    created postit component

---
+++ src/app/components/postit/postit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-postit',
+  templateUrl: './postit.component.html',
+  styleUrls: ['./postit.component.scss'],
+})
+export class PostitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
